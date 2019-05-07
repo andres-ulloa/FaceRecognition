7bd5134208e8a84618b8214b208189cfd40a0e92
andres <andresulloa97@gmail.com>
2019-05-07 12:32:29 -0500

    classifier flow

--- classifier.py
+++ classifier.py
@@ -2,14 +2,29 @@ import numpy as np
 import math
 from sklearn.svm import SVC
 
+def build_svm_model():
+    pass
+
 def classify():
     pass
 
 def load_embeddings():
     pass
 
-def main():
+def compute_confution_matrix(labels):
     pass
+
+def main():
+    
+    embeddings = load_embeddings()
+    svm = build_svm_model()
+    labels = classify()
+    confution_matrix =  compute_confution_matrix(labels)
+    print('\n\n--------------------------------------------------------------------------------')
+    print('-------------------------GENERATING ROC METRICS----------------------------------')
+    print('--------------------------------------------------------------------------------\n\n')
+    print(confution_matrix)
+
 
 
 if __name__ == '__main__':
