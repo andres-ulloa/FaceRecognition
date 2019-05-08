39de461935d2fa8fd8f77b9678e94ab61f3e69ca
andres <andresulloa97@gmail.com>
2019-05-08 13:29:21 -0500

    non processed dataset

--- classifier.py
+++ classifier.py
@@ -2,8 +2,14 @@ import numpy as np
 import math
 from sklearn.svm import SVC
 
-def build_svm_model():
-    pass
+def _train_and_save_classifier(emb_array, label_array, class_names, classifier_filename_exp):
+    logger.info('Training Classifier')
+    model = SVC(kernel='linear', probability=True, verbose=False)
+    model.fit(emb_array, label_array)
+    with open(classifier_filename_exp, 'wb') as outfile:
+        pickle.dump((model, class_names), outfile)
+    logging.info('Saved classifier model to file "%s"' % classifier_filename_exp)
+    
 
 def classify():
     pass
@@ -15,7 +21,7 @@ def compute_confution_matrix(labels):
     pass
 
 def main():
-    
+
     embeddings = load_embeddings()
     svm = build_svm_model()
     labels = classify()
