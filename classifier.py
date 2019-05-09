ecabd50c66bfd03e5d986d0689cddc76f9970b88
andres <andresulloa97@gmail.com>
2019-05-09 10:47:56 -0500

    raw test and training set

--- classifier.py
+++ classifier.py
@@ -2,29 +2,33 @@ import numpy as np
 import math
 from sklearn.svm import SVC
 
-def _train_and_save_classifier(emb_array, label_array, class_names, classifier_filename_exp):
+def train_and_save_classifier(emb_array, label_array, class_names, classifier_filename_exp):
     logger.info('Training Classifier')
-    model = SVC(kernel='linear', probability=True, verbose=False)
+    model = SVC(kernel='linear', probability = True, verbose = False)
     model.fit(emb_array, label_array)
     with open(classifier_filename_exp, 'wb') as outfile:
         pickle.dump((model, class_names), outfile)
     logging.info('Saved classifier model to file "%s"' % classifier_filename_exp)
+
+
+def test_classifier(model, test_set):
+    pass
+
+def load_embeddings(path_file):
+    pass
+
+
+def load_model_parameters(file_path):
+    pass
+
     
-
-def classify():
-    pass
-
-def load_embeddings():
-    pass
-
 def compute_confution_matrix(labels):
     pass
 
 def main():
 
     embeddings = load_embeddings()
-    svm = build_svm_model()
-    labels = classify()
+    svm = train_and_save_classifier(embeddings, labels, class_names, 'training_results.csv')
     confution_matrix =  compute_confution_matrix(labels)
     print('\n\n--------------------------------------------------------------------------------')
     print('-------------------------GENERATING ROC METRICS----------------------------------')
